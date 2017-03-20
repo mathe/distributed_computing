b87829d589a4622e58ebcc21c03e04b901d21027
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-20 16:54:08 -0300

    Simple single server chat.

---
+++ single_server_chat/server.py
@@ -0,0 +1,40 @@
+from bottle import run, get, post, error
+
+class Chat:
+    def __init__(self):
+        self.messages = {}    
+        
+    def get_messages(self,f,to):    
+        if f in self.messages and to in self.messages[f]: return self.messages[f][to]
+        return ["There is no message from " + f + " to " + to + "."]    
+        
+    def set_message(self,f,to,msg):        
+        if f not in self.messages: self.messages[f] = {}
+        if to not in self.messages[f]: self.messages[f][to] = []
+        self.messages[f][to].append(msg)
+
+@get("/msg_history/<f>/<to>") 
+def msg_history(f,to): 
+    global chat
+    
+    history = ""
+    messages = chat.get_messages(f,to)
+    for mi in messages:
+        history += mi + "<br/>"
+        
+    return history
+
+@post("/send_msg/<f>/<to>/<msg>")
+def send_msg(f,to,msg):
+    global chat
+    print(f + " " + to + " " + msg)
+    #chat.set_message(f,to,msg)
+    print(f + " " + to + " " + msg)
+    return "Message sent!"
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
+chat = Chat()
+run(host="localhost", port=8080, debug = True)
